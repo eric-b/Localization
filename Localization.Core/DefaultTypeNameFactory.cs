0fd68b1e6d2acdb44da515a8b26a465ecc9bccc2
Eric Boumendil <eric.bml@gmail.com>
2013-05-18 13:30:51 +0200

    DefaultTypeNameFactory: friendly name for generic types (removes assembly qualified name of generic types).
    Note: this change breaks existing keys based on source names (generic types only).
    
    	modified:   Localization.Core/DefaultTypeNameFactory.cs

--- Localization.Core/DefaultTypeNameFactory.cs
+++ Localization.Core/DefaultTypeNameFactory.cs
@@ -9,6 +9,10 @@ namespace Localization.Core
     {
         public string GetSourceName(Type type)
         {
+            // Special treatment for generic types (removes assembly qualified name of parameter types).
+            if (type.IsGenericType)
+                return ToGenericTypeString(type);
+
             var fullname = type.FullName;
 
             #region ImpromptuInterface special case
@@ -16,7 +20,7 @@ namespace Localization.Core
             const string actLikePrefix = "ActLike_";
             if (fullname.StartsWith(actLikePrefix))
             {
-                // Ex: "ActLike_IStep1_a2c76f01ac234addaf7deb40dbf4f2bc" -> "ActLike_IStep1_generated"
+                // Ex: "ActLike_IMyInterface_a2c76f01ac234addaf7deb40dbf4f2bc" -> "ActLike_IMyInterface_generated"
                 var index = fullname.LastIndexOf('_');
                 if (index > actLikePrefix.Length)
                     fullname = fullname.Substring(0, index) + generatedSuffix;
@@ -24,6 +28,22 @@ namespace Localization.Core
             #endregion
 
             return fullname;
+        }
+
+        /// <summary>
+        /// <para>Returns a friendly name for generic types.
+        /// Example: "My.GenericType`1[[My.ParameterType, My.App, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null]]" -> "My.GenericType&lt;My.ParameterType>"</para>
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        protected string ToGenericTypeString(Type type)
+        {
+            if (!type.IsGenericType)
+                return type.FullName;
+            var genericTypeName = type.GetGenericTypeDefinition().FullName;
+            genericTypeName = genericTypeName.Substring(0, genericTypeName.IndexOf('`'));
+            var genericArgs = string.Join(", ", type.GetGenericArguments().Select(ToGenericTypeString));
+            return genericTypeName + "<" + genericArgs + ">";
         }
     }
 }
